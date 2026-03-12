6edd2f08c5f902eca00365cd8b47c0380f287a8d
SAM-RANJITH <141648923+SAM-RANJITH@users.noreply.github.com>
2026-03-12 13:17:40 +0530

    Update envelope_detector.v

--- src/envelope_detector.v
+++ src/envelope_detector.v
@@ -1,17 +1,20 @@
 `default_nettype none
 
 module envelope_detector(
+
     input wire clk,
     input signed [7:0] signal,
     output reg [7:0] env
+
 );
 
-always @(posedge clk)
-begin
+always @(posedge clk) begin
+
     if(signal < 0)
         env <= -signal;
     else
         env <= signal;
+
 end
 
 endmodule
