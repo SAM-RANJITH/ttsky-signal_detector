04d9d48d81c97ded42dac0abc51c6c38029eb00c
SAM-RANJITH <141648923+SAM-RANJITH@users.noreply.github.com>
2026-03-12 13:16:57 +0530

    Update fir_filter.v

--- src/fir_filter.v
+++ src/fir_filter.v
@@ -1,16 +1,18 @@
 `default_nettype none
 
 module fir_filter(
+
     input wire clk,
     input wire [7:0] din,
     output reg [7:0] dout
+
 );
 
 reg [7:0] d1,d2,d3;
 reg [9:0] acc;
 
-always @(posedge clk)
-begin
+always @(posedge clk) begin
+
     d3 <= d2;
     d2 <= d1;
     d1 <= din;
@@ -18,6 +20,7 @@ begin
     acc = din + (d1<<1) + (d2<<1) + d3;
 
     dout <= acc[9:2];
+
 end
 
 endmodule
