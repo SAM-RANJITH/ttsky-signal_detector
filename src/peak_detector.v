d2712b7da5be9d16dca7e4d362ed5db79bc6f782
SAM-RANJITH <141648923+SAM-RANJITH@users.noreply.github.com>
2026-03-12 13:18:03 +0530

    Update peak_detector.v

--- src/peak_detector.v
+++ src/peak_detector.v
@@ -1,19 +1,22 @@
 `default_nettype none
 
 module peak_detector(
+
     input wire clk,
     input wire [7:0] signal,
     output reg detect
+
 );
 
 parameter THRESHOLD = 8'd120;
 
-always @(posedge clk)
-begin
+always @(posedge clk) begin
+
     if(signal > THRESHOLD)
         detect <= 1'b1;
     else
         detect <= 1'b0;
+
 end
 
 endmodule
